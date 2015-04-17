5a93a9a3978c15590bf9a7853e322fb85d6ed9fb
Chirag Dixit <cdixi001@hammer.cs.ucr.edu>
2015-04-16 22:37:53 -0700

    tokenizing wit spaces

--- src/main.cpp
+++ src/main.cpp
@@ -3,47 +3,59 @@
 #include <errno.h>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cstring>
 
 using namespace std;
+
+vector<char*> parser(string text) {
+	char *line = new char[text.length()+1];	//strtok requires char*
+	strcpy(line, text.c_str());
+
+
+	char *token = strtok(line, " ");	//can be any character
+	vector<char*> words;
+
+	words.push_back(token);
+
+	while(token != NULL) {	
+		words.push_back(token);
+		token = strtok(NULL, " ");
+	}
+	
+	delete[] token;
+	
+	return words;
+}
+
+
 
 void terminal() {
 	char host[32];//add to readme: 32 cuz max is 64 and i dont want long printing
 	int pid;
+	
 	string arg;
 	
 	pid = gethostname(host, 32);
-	if(pid == -1) {
+	if(pid == -1) {				//error
 		perror("gethostname");
 	} else {
-		 cout << getlogin() << '@' << host << ':';
-		pid = fork();
-		if(pid == -1) {
-			perror("fork");
-		} else if(pid == 0){
-			if(-1 == execvp("pwd",NULL)) {
-				perror("error in execvp");
-			}
-		} else {
-			cout << "dask" << endl;		
-		}
+		 cout << getlogin() << '%' << host << ':' << ' ';
 	}	
+	
+	getline(cin, arg);	
+	vector<char*> words = parser(arg);
 
-	cin >> arg;
-
-	if(arg == "penis") {
-		cout << ' ' << '|' << '\n';
-		cout << ' ' << '|' << '\n';
-		cout << ' ' << '|' << '\n';
-		cout << 'o' << '|' << 'o' << endl;
-	} else
-
-	cout << arg << endl;
-
+	//TESTING DELETE THIS LATER
+	for(int i = 0; i < words.size(); i++) {
+		cout << words.at(i) << endl;
+	}
 }
 
 int main(int argc, char* argv[]) {
 
 	cout << '$' << endl;
+
 	terminal();
 
 	return 0;
