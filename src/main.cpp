877a986d4b4113fdfdedb6e596a70d70051208cb
Chirag Dixit <cdixi001@hammer.cs.ucr.edu>
2015-04-18 19:20:55 -0700

    seems to workish. only first command executes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/wait.h>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <cstring>
+#include <cstdlib>
 
 using namespace std;
 
@@ -47,6 +49,7 @@ vector<char*> parser(string text, const char *delim) {
 
 
 void terminal() {
+	//~~~~~~~~~~~~print username, hostnam, and prompte~~~~~~~~
 	char host[32];//add to readme: 32 cuz max is 64 and i dont want long printing
 	int pid;
 	
@@ -57,12 +60,15 @@ void terminal() {
 		perror("gethostname");
 	} else {
 		 cout << getlogin() << "§" << host << ':' << ' ';
-	}	
+	}
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	
+	//shit starts here
+	//we will begin tonight's program by taking in 
 	getline(cin, arg);
 	cout << arg << endl;
 	
-	//operator stuff
+	//add spaces before and after all operators in arg
 	addSpaces(arg, ";");
 	addSpaces(arg, "&&");
 	addSpaces(arg, "||");
@@ -70,19 +76,57 @@ void terminal() {
 	cout << arg << endl;	//testing if addSpaces actually works..
 	
 	vector<char*> words;
-	words = parser(arg, " ");
-	//words.push_back(NULL); //don't need. vector already null terminanted
+	words = parser(arg, " "); //take out all spaces from arg
+	words.push_back(NULL);
+
+
+
+
 
 	char **command = &words[0]; //stackoverflow.com/questions/5846934/
+
+	vector<char*> onecommand;
 	
+	size_t i = 0;
+	for(i = 0; strcmp(words.at(i), ";") && strcmp(words.at(i), "&&") &&  strcmp(words.at(i), "||"); i++) {
+		cout << "theo1";
+		onecommand.push_back(words.at(i));
+	}
+	
+	//deleteme printig onecommand for testing
+	for(int i = 0; i < onecommand.size(); i++)
+		cout << "onecommand" << i << onecommand.at(i) << endl;
+	command = &onecommand[0];
+			
 	//EXECVP STUFF HERE
-	//int status = execvp(command[0], command);
+	//http://github.com/mikeizbicki/ucr-cs100/blob/2015spring/textbook/assignment-help/syscalls/exec.md
+
+	pid = fork();
+	if(pid == -1) {
+		perror("Error with fork");
+		exit(1);
+	} else if(pid == 0) {	//child process
+		if(-1 == execvp(command[0], command)) {
+			perror("Error wit execvp");
+		}
+
+		exit(1);
+	}
+	if(-1 == wait(0)) {
+		perror("wait error");
+	}
+	//parent here
+	
+
+
+
+
+
 	
 	
 	//TESTING DELETE THIS LATER
 	for(int i = 0; i < words.size(); i++) {
-		cout << i;
-		cout <<  words.at(i) << endl;
+		cout << "words" << i << words.at(i) << endl;
 	}
 }
 
