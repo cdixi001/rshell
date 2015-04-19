05d00ec17aadf64ce546ad1070e3d5d6d4a1d084
Chirag Dixit <cdixi001@hammer.cs.ucr.edu>
2015-04-19 00:03:53 -0700

    seg fault. stuff doesn't work

--- src/main.cpp
+++ src/main.cpp
@@ -26,26 +26,27 @@ void addSpaces(string &text, const string &op) {
 }
 
 //takes in string and deliminator and tokenizes that string into vector<char*>
+/*
 vector<char*> parser(string text, const char *delim) {
-	char *line = new char[text.length()+1];	//strtok requires char*
-						//got this from cplusplus.com/reference/string/string/c_str
+	char *line = new char[arg.length()+1];	//strtok requires char*					//got this from cplusplus.com/reference/string/string/c_str
+	vector<char*> words;
 	strcpy(line, text.c_str());
 
 
 	char *token = strtok(line, delim);	//can be any character
-	vector<char*> words;
 
 
 	while(token != NULL) {	
+		cout << "toekn" << token << endl;
 		words.push_back(token);
 		token = strtok(NULL, delim);
 	}
-	
-	delete[] token;
-	
+cout << words.at(0);	
+cout << words.at(0);
+	delete[] line;		//this goddamn line messig it up
 	return words;
 }
-
+*/
 
 
 void terminal() {
@@ -76,58 +77,107 @@ void terminal() {
 	cout << arg << endl;	//testing if addSpaces actually works..
 	
 	vector<char*> words;
-	words = parser(arg, " "); //take out all spaces from arg
-	words.push_back(NULL);
+	char delim[] = " ";
+
+	//~~~~~~~~~~~~PARSER~~~~~~~~~~~~~~~~~~
+//used to be a bbeautiful function then stupid delete crap messed it upu
+	char *line = new char[arg.length()+1];	//strtok requires char*					//got this from cplusplus.com/reference/string/string/c_str
+	strcpy(line, arg.c_str());
 
 
+	char *token = strtok(line, delim);	//can be any character
 
 
+	while(token != NULL) {	
+		words.push_back(token);
+		token = strtok(NULL, delim);
+	}
+
+
+	//~~~~~~~~~~~~END PARSER~~~~~~~~~~~~~~~~
+
+	//words = parser(arg, " "); //take out all spaces from arg
+	words.push_back('\0');
+	
+
+	cout << "theo5" << endl;
 
 	char **command = &words[0]; //stackoverflow.com/questions/5846934/
+	cout << "theo6" << endl;	
 
 	vector<char*> onecommand;
 	
-	size_t i = 0;
-	for(i = 0; strcmp(words.at(i), ";") && strcmp(words.at(i), "&&") &&  strcmp(words.at(i), "||"); i++) {
-		cout << "theo1";
+	unsigned int i = 0;
+	//bool issemi = strcmp(words.at(i), ";") == 0;
+	//bool isand = strcmp(words.at(i), "&&") == 0;
+	//bool isor = strcmp(words.at(i), "||") == 0;
+	//bool isoperator = issemi || isand || isor;
+	bool prev = 1;
+	//bool curr = 1;
+	bool test = i < words.size();
+	cout << "isoperator" << isoperator << endl;
+
+	while(i < words.size()){
+		cout << "theo1" << test;
 		onecommand.push_back(words.at(i));
-	}
+		i++;
 	
-	//deleteme printig onecommand for testing
-	for(int i = 0; i < onecommand.size(); i++)
-		cout << "onecommand" << i << onecommand.at(i) << endl;
-	command = &onecommand[0];
+	//	issemi = strcmp(words.at(i), ";") == 0;
+	//	isand = strcmp(words.at(i), "&&") == 0;
+	//	isor = strcmp(words.at(i), "||") == 0;
+	//	isoperator = issemi || isand || isor;
+		cout << "theo3" << endl;
+	}
+	cout << "theo4" << endl;
+	if(1) {
+		if(issemi) {
+			i++;
+		}
+		if(isand && prev) {
+			i++;
+		}
+		if(isor && !prev) {
+			i++;
+		}
+	} else {
+		
+		//deleteme printig onecommand for testing
+		for(unsigned int i = 0; i < onecommand.size(); i++)
+			cout << "onecommand" << i << onecommand.at(i) << endl;
+		command = &onecommand[0];
 			
 	//EXECVP STUFF HERE
 	//http://github.com/mikeizbicki/ucr-cs100/blob/2015spring/textbook/assignment-help/syscalls/exec.md
 
-	pid = fork();
-	if(pid == -1) {
-		perror("Error with fork");
-		exit(1);
-	} else if(pid == 0) {	//child process
-		if(-1 == execvp(command[0], command)) {
-			perror("Error wit execvp");
+		pid = fork();
+		if(pid == -1) {
+			prev = false;
+			perror("Error with fork");
+			exit(1);
+		} else if(pid == 0) {	//child process
+			if(-1 == execvp(command[0], command)) {
+				prev = false;
+				perror("Error wit execvp");
+				exit(1);
+			}
+			prev = true;
+			exit(1);
 		}
+		if(-1 == wait(0)) {
+			perror("wait error");
+		}
+		//parent here
 
-		exit(1);
+
 	}
-	if(-1 == wait(0)) {
-		perror("wait error");
-	}
-	//parent here
-	
+		onecommand.clear(); //onto the next command!
+		
 
-
-
-
-
-	
+	delete[] line;
 	
 	//TESTING DELETE THIS LATER
-	for(int i = 0; i < words.size(); i++) {
-		cout << "words" << i << words.at(i) << endl;
-	}
+//	for(int i = 0; i < words.size(); i++) cout << "words" << i << words.at(i) << endl;
+	
 }
 
 int main(int argc, char* argv[]) {
