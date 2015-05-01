52bba21ec67231240400809866187a134a0bd443
Chirag Dixit <cdixi001@hammer.cs.ucr.edu>
2015-05-01 16:11:10 -0700

    ls foldername doesn't work. trying to add -R

--- src/hw1.cpp
+++ src/hw1.cpp
@@ -6,40 +6,119 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <errno.h>
+#include <string.h>
+#include <vector>
 
 using namespace std;
 
-void printdir( char* dir, const bool &l, const bool &a, const bool &R) {
+void printdir(struct stat s, const char* dir, const bool &l, const bool &a, const bool &R) {
+	cout << "current directory: " << dir << endl;
+	vector<const char*> subdirs;
+	DIR *dirp;
+	if(NULL == (dirp = opendir(dir))) {
+		perror("error with oepndir(). ");
+		exit(1);
+	}
+	struct dirent *filespecs;
+	errno = 0;
+
 	if(l) {
 		//l stuff here
-	} else {
-		//non l stuff
-		DIR *dirp;
-		if(NULL == (dirp = opendir(dir))) {
-			perror("error with oepndir(). ");
+	while(NULL != (filespecs = readdir(dirp))) {	
+		if(a || (filespecs->d_name[0] != '.')) {
+		
+		if(-1 == stat(filespecs->d_name, &s)) {
+			perror("error with stat");
+			cout << "could not find file or directory" << endl;
 			exit(1);
 		}
-		struct dirent *filespecs;
-		errno = 0;
-		while(NULL != (filespecs = readdir(dirp))) {
-			if(filespecs->d_name[0] == '.') {
-				if(a) {
-					cout << filespecs->d_name << ' ';
-				}
-			} else cout << filespecs->d_name << ' ';
+ 
+		if(S_IFDIR & s.st_mode && (strcmp(filespecs->d_name, ".") != 0 && strcmp(filespecs->d_name, "..") != 0)) {
+			cout << "d";
+			char subdir[strlen(dir) + strlen(filespecs->d_name) + 1];
+			strcpy(subdir, dir);
+			strcat(subdir, "/");
+			strcat(subdir, filespecs->d_name);
+			subdirs.push_back(subdir);
+			//char* vector stuff here with path appended or whatever
+		} else cout << '-';
+
+		cout	<< ((S_IRUSR & s.st_mode) ? "r":"-")
+			<< ((S_IWUSR & s.st_mode) ? "w":"-")
+			<< ((S_IXUSR & s.st_mode) ? "x":"-")
+			<< ((S_IRGRP & s.st_mode) ? "r":"-")
+			<< ((S_IWGRP & s.st_mode) ? "w":"-")
+			<< ((S_IXGRP & s.st_mode) ? "x":"-")
+			<< ((S_IROTH & s.st_mode) ? "r":"-")
+			<< ((S_IWOTH & s.st_mode) ? "w":"-")
+			<< ((S_IXOTH & s.st_mode) ? "x":"-")
+			<< ' '
+			<< s.st_nlink << ' '
+			<< s.st_uid << ' ' << s.st_gid << ' '
+			<< s.st_size << ' ' //some block crap here
+			<< filespecs->d_name
+			<< endl;
 		}
-		if(errno != 0) {
-			perror("error with readdir()");
-			exit(1);
-		}
-		cout << endl;
-		if(-1 == closedir(dirp)) {
-			perror("error with closedir()");
-			exit(1);
 		}
 
+
+
+	} else {
+		//non l stuff
+
+
+
+		while(NULL != (filespecs = readdir(dirp))) {
+			if(-1 == stat(filespecs->d_name, &s)) {
+				
+			cout << "theo" << filespecs->d_name;
+				perror("error with stat");
+				cout << "not l: could not find file or directory" << endl;
+				exit(1);
+			}
+ 			if(S_IFDIR & s.st_mode &&
+			(strcmp(filespecs->d_name, ".") != 0 &&
+			strcmp(filespecs->d_name, "..") != 0)) {
+				//char* vector stuff here with path appended or whatever
+				string currentdirectory = dir;
+				string dname = filespecs->d_name;
+				string sub = currentdirectory + '/' + dname;
+				subdirs.push_back(sub.c_str());
+			}
+
+			if(a || filespecs->d_name[0] != '.') {
+				cout << filespecs->d_name << ' ';
+			}
+		}
+
+
+
+	}
+
+	if(errno != 0) {
+		perror("error with readdir()");
+		exit(1);
+	}
+	cout << endl;
+	if(-1 == closedir(dirp)) {
+		perror("error with closedir()");
+		exit(1);
 	}
 	//recursion stuff here?
+	if(R) {
+		cout << endl;
+		for(int i = 0; i < subdirs.size(); i++) {
+			struct stat blah;
+			if(-1 == stat(subdirs.at(i), &blah)) {
+				perror("error with stat");
+				cout << "could not find file or directory" << endl;
+				exit(1);
+			}
+			cout << subdirs.at(i) << ':' << endl;
+			printdir(blah, subdirs.at(i), l, a, R);
+		}
+	}
+		
 }
 
 void getFlags(const int argc, char** &args, bool &l, bool &a, bool &R, bool &specified) {
@@ -101,7 +180,7 @@ int main(int argc, char** argv) {
 			if(S_IFDIR & s.st_mode) {	//it's a directory
 				//do directory print stuff here
 				//see dir_code.cpp file
-				printdir(argv[i], el, ay, ar);
+				printdir(s, argv[i], el, ay, ar);
 
 			} else {			//it's a file
 				//
@@ -112,7 +191,13 @@ int main(int argc, char** argv) {
 		}
 	}
 	} else {
-		printdir(dot, el, ay, ar);
+		cout << "nothing specified" << endl;
+		if(-1 == stat(dot, &s)) {
+			perror("error with stat");
+			cout << "could not find file or directory" << endl;
+			exit(1);
+		}
+		printdir(s, dot, el, ay, ar);
 	}
 
 	return 0;
