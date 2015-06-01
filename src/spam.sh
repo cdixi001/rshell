2f010d975a7e6c826956e13f22d042fb20f84fa4
Chirag Dixit <cdixi001@hammer.cs.ucr.edu>
2015-06-01 15:41:50 -0700

    lab8 spam bash script

---
+++ src/spam.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+#read recipient sender
+website=`echo $2 | cut -d '@' -f 2`
+
+echo "recipiennt $1 sender $2"
+
+(
+sleep 3
+echo "helo $website"
+sleep 2
+echo "mail from: $2"
+sleep 2
+echo "rcpt to: $1"
+sleep 2
+echo "data"
+sleep 2
+echo "Subject: I'm da captain"
+sleep 2
+echo "i'm da captain now."
+sleep 2
+echo "."
+) | 
+telnet mail.cs.ucr.edu 25
